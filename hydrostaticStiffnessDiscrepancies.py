Added displacement field calculation to springing beam. Tried to fix hydrostatics calculation, only made it worse. I'm very tired, boss.

hydrostaticStiffnessDiscrepancies.py
@@ -6,7 +6,7 @@ np.set_printoptions(linewidth=100000)
 
 hullLength = 1
 hullBreadth = 1
-hullDraft = 1
+hullDraft = 0.5
 
 
 
@@ -14,24 +14,48 @@ hullSize = (hullLength, hullBreadth, hullDraft)
 hullCenter = (0, 0, 0)
 meshResolution = (10, 10, 5)
 
-hullMesh = cpt.mesh_parallelepiped(size = hullSize, center = hullCenter, name = 'hull', resolution = meshResolution).immersed_part()
+hullMesh: cpt.Mesh = cpt.mesh_parallelepiped(size = hullSize, center = hullCenter, name = 'hull', resolution = meshResolution).immersed_part()
 
 rigidBodyDofs = cpt.rigid_body_dofs(hullCenter)
 rigidBody = cpt.FloatingBody(mesh = hullMesh, dofs = rigidBodyDofs, center_of_mass = hullCenter)
 rigidBodyStiffnessMatrix = rigidBody.compute_hydrostatic_stiffness()
 
+# myDofs = {}
+# myDofs['x'] = np.zeros([hullMesh.nb_faces, 3])
+# myDofs['x'][:, 0] = 1
+# myDofs['p'] = np.zeros([hullMesh.nb_faces, 3])
+# myDofs['p'][:, 2] = -hullMesh.faces_centers[:, 0]
+# myDofs['p'][:, 0] = hullMesh.faces_centers[:, 2]
+
+freeSurfaceVertices = np.isclose(hullMesh.vertices[:, 2], 0)
+
+vertexDofs = {}
+vertexDofs['x'] = np.zeros([hullMesh.nb_vertices, 3])
+vertexDofs['x'][:, 0] = 1
+vertexDofs['x'][freeSurfaceVertices, 2] = 0
+vertexDofs['p'] = np.zeros([hullMesh.nb_vertices, 3])
+vertexDofs['p'][:, 2] = -hullMesh.vertices[:, 0]
+vertexDofs['p'][:, 0] = hullMesh.vertices[:, 2]
+vertexDofs['p'][freeSurfaceVertices, 2] = 0
+
 myDofs = {}
-myDofs['x'] = np.zeros([hullMesh.nb_faces, 3])
-myDofs['x'][:, 0] = 1
-myDofs['p'] = np.zeros([hullMesh.nb_faces, 3])
-myDofs['p'][:, 2] = hullMesh.faces_centers[:, 0]
+myDofs['x'] = np.mean(vertexDofs['x'][hullMesh.faces, :], axis = 1)
+myDofs['p'] = np.mean(vertexDofs['p'][hullMesh.faces, :], axis = 1)
+
+dofJacobians = {}
+dofJacobians['x'] = np.zeros([hullMesh.nb_faces, 3, 3])
+dofJacobians['p'] = np.zeros([hullMesh.nb_faces, 3, 3])
+dofJacobians['p'][:, 0, 2] = 1
+dofJacobians['p'][:, 2, 0] = -1
 
 myBody = cpt.FloatingBody(mesh = hullMesh, dofs = myDofs, center_of_mass = hullCenter)
 myBodyStiffnessMatrix = myBody.compute_hydrostatic_stiffness()
-myMethodStiffnessMatrix = spr.ComputeHydrostaticStiffness(myBody, 1000, 9.81)
+myFunctionStiffnessMatrix = spr.ComputeHydrostaticStiffness(myBody, 1000, 9.81)
+myFunctionMyMethodStiffnessMatrix = spr.ComputeHydrostaticStiffnessNewMethod(myBody, vertexDofs, dofJacobians, 1000, 9.81)
 
 
 
 print(rigidBodyStiffnessMatrix)
 print(myBodyStiffnessMatrix)
-print(myMethodStiffnessMatrix)
+print(myFunctionStiffnessMatrix)
+print(myFunctionMyMethodStiffnessMatrix)

test.py
@@ -1,8 +1,7 @@
 import numpy as np
-from scipy.integrate import solve_bvp
-import matplotlib.pyplot as plt
 import springing as spr
 import capytaine as cpt
+import time as tm
 
 
 
@@ -61,88 +60,23 @@ mesh = cpt.mesh_parallelepiped(size = meshSize, center = meshCenter, name = 'hul
 
 beam = spr.Beam(beamDefinition)
 
-dryNaturalFrequenciesSquared, dryVibrationModesNormalized, modalDofs = beam.CalculateModalDOFs(mesh, 100)
+tic = tm.time()
+nodalDofsOld = beam.CalculateNodalDOFs(mesh)
+tac = tm.time()
+elapsedTimeOldFunction = tac - tic
 
+tic = tm.time()
+nodalDofsNew = beam.CalculateNodalDOFsNew(mesh)
+tac = tm.time()
+elapsedTimeNewFunction = tac - tic
 
-def BendingTorsionODEFunction(x: np.ndarray, y: np.ndarray, p: np.ndarray):
-    h = y[0, :]
-    hPrime = y[1, :]
-    hDoublePrime = y[2, :]
-    hTriplePrime = y[3, :]
-    a = y[4, :]
-    aPrime = y[5, :]
+maxError = 0
 
-    omegaSquared = p[0]
+for dofName in nodalDofsOld.keys():
+    errors = np.abs(nodalDofsNew[dofName] - nodalDofsOld[dofName])
+    if np.max(errors) > maxError:
+        maxError = np.max(errors)
 
-    yPrime = np.zeros(y.shape)
-    yPrime[0, :] = hPrime
-    yPrime[1, :] = hDoublePrime
-    yPrime[2, :] = hTriplePrime
-    yPrime[3, :] = omegaSquared * linearDensity / (youngsModulus * horizontalAreaMoment) * (h - axesOffset * a)
-    yPrime[4, :] = aPrime
-    yPrime[5, :] = omegaSquared / (shearModulus * torsionConstant) * (axesOffset * linearDensity * h - (axesOffset**2 * linearDensity + rollInertiaDensity) * a)
-
-    return yPrime
-
-
-def BendingTorsionBC(yInitial: np.ndarray, yFinal: np.ndarray, p: np.ndarray):
-    boundaryConditions = np.zeros([7])
-
-    boundaryConditions[0] = yInitial[2]
-    boundaryConditions[1] = yInitial[3]
-    boundaryConditions[2] = yInitial[5]
-    boundaryConditions[3] = yFinal[2]
-    boundaryConditions[4] = yFinal[3]
-    boundaryConditions[5] = yFinal[5]
-    boundaryConditions[6] = yInitial[4] + 1
-
-    return boundaryConditions
-
-
-xGuess = np.linspace(0, beamLength, initialNodes)
-yGuess = np.zeros([6, initialNodes])
-
-bendingCoef = bendingNodes * np.pi / beamLength
-torsionCoef = torsionNodes * np.pi / beamLength
-
-yGuess[0, :] = -axesOffset * np.cos(bendingCoef * xGuess)
-yGuess[1, :] = axesOffset * bendingCoef * np.sin(bendingCoef * xGuess)
-yGuess[2, :] = axesOffset * bendingCoef**2 * np.cos(bendingCoef * xGuess)
-yGuess[3, :] = -axesOffset * bendingCoef**3 * np.sin(bendingCoef * xGuess)
-yGuess[4, :] = -np.cos(torsionCoef * xGuess)
-yGuess[5, :] = torsionCoef * np.sin(torsionCoef * xGuess)
-
-hQuadruplePrimeGuess = -axesOffset * bendingCoef**4 * np.cos(bendingCoef * xGuess)
-hQuadruplePrimeODEFromGuessOverOmegaSquared = linearDensity / (youngsModulus * horizontalAreaMoment) * (yGuess[0, :] - axesOffset * yGuess[4, :])
-
-aDoublePrimeGuess = torsionCoef**2 * np.cos(torsionCoef * xGuess)
-aDoublePrimeODEFromGuessOverOmegaSquared = 1 / (shearModulus * torsionConstant) * (axesOffset * linearDensity * yGuess[0, :] - (axesOffset**2 * linearDensity + rollInertiaDensity) * yGuess[4, :])
-
-omegaSquaredGuess = np.sum(aDoublePrimeGuess * aDoublePrimeODEFromGuessOverOmegaSquared) / np.sum(aDoublePrimeODEFromGuessOverOmegaSquared**2)
-
-solution = solve_bvp(BendingTorsionODEFunction, BendingTorsionBC, xGuess, yGuess, np.array([omegaSquaredGuess]))
-
-ySolution = solution.sol(xGuess)
-
-
-
-print('Omega squared guess:')
-print(omegaSquaredGuess)
-print('Omega squared:')
-print(solution.p[0])
-
-plt.figure()
-plt.plot(xGuess, hQuadruplePrimeGuess)
-plt.plot(xGuess, hQuadruplePrimeODEFromGuessOverOmegaSquared, '--')
-
-plt.figure()
-plt.plot(xGuess, aDoublePrimeGuess)
-plt.plot(xGuess, aDoublePrimeODEFromGuessOverOmegaSquared, '--')
-
-plt.figure()
-plt.plot(xGuess, yGuess[0, :], 'b--')
-plt.plot(xGuess, yGuess[4, :], 'g--')
-plt.plot(xGuess, ySolution[0, :])
-plt.plot(xGuess, ySolution[4, :], '--')
-
-plt.show()
+print('Old function: %f s'%elapsedTimeOldFunction)
+print('New function: %f s'%elapsedTimeNewFunction)
+print('Max error: %e'%maxError)